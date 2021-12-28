f0f8c1b071f4a0c72d7209f8ca3e771d1f5e8b04
marcos <marcos.silvestrini@gmail.com>
2021-12-27 21:18:19 -0300

    fix: Fix script up-stack

--- linux/scripts/up-stack.sh
+++ linux/scripts/up-stack.sh
@@ -2,31 +2,31 @@
 cd /etc/init.d
 
 echo '[Start Lock Server]'
-./totvslockserver start
+nohup ./totvslockserver start &
 sleep 5
 
 echo '[Start License Server]'
-./totvslicense start
+nohup ./totvslicense start &
 sleep 2
 
 echo '[Start DBAccess Server]'
-./totvsdbaccess start
+nohup ./totvsdbaccess start
 sleep 2
 
 echo '[Start Broker Server]'
 cd /totvs/bin/appserver/broker
-./app_broker -balance_smart_client_desktop -a
+nohup ./app_broker -balance_smart_client_desktop -a &
 
 cd /etc/init.d
 
 echo '[Start Slave01]'
-./totvslave01 start
+nohup ./totvslave01 start &
 
 echo '[Start Slave02]'
-./totvslave02 start
+nohup ./totvslave02 start &
 
 echo '[Start Soap]'
-./totvssoap start
+nohup ./totvssoap start &
 
 echo '[Start Rest]'
-./totvsrest start
+nohup ./totvsrest start &
